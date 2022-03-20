bb048b96828847ced1028b247e1a4a26db4a11b7
aquib-sh <shaikhaquib394@gmail.com>
2022-03-20 20:37:07 +0530

    Add browser module with search feature

---
+++ browser.py
@@ -0,0 +1,31 @@
+from bot import BotMaker
+import config
+
+class Browser:
+    def __init__(self):
+        self.__kernel = BotMaker(browser='Chrome')
+        self.xpaths = config.MAPPINGS['xpaths']
+
+    def goto(self, url):
+        self.__kernel.move(url)
+
+    def goto_homepage(self):
+        self.goto(config.HOMEPAGE)
+
+    def search(self, term, log=1):
+        search_box = self.__kernel.get_element(
+                self.xpaths['amazon_product_search_box'])
+        search_btn = self.__kernel.get_element(
+                self.xpaths['amazon_product_search_btn'])
+
+        if log: print(f"[+] Searching {term}")
+        self.__kernel.send_human_keys(search_box, term)
+        search_btn.click()
+
+    def close(self):
+        self.__kernel.shutdown()
+
+if __name__ == "__main__":
+    browser = Browser()
+    browser.goto_homepage()
+    browser.search("walnuts")
