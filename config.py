6877572bef8b8f859d18216ec48e50525d3c8dd1
aquib-sh <shaikhaquib394@gmail.com>
2022-03-20 20:36:49 +0530

    Add configuration file

---
+++ config.py
@@ -0,0 +1,11 @@
+HOMEPAGE = "https://www.amazon.in"
+
+MAPPINGS = {
+        "xpaths":
+        {
+            "amazon_product_search_box":"//input[@type='text'][@id='twotabsearchtextbox']",
+            "amazon_product_search_btn":"//input[@type='submit'][@id='nav-search-submit-button']"
+        },
+
+        "selectors":{}
+}
